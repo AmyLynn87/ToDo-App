9f7b96ee7142da887fef478bd86df9361f8b1622
AmyLynn87 <e.crescentini87@gmail.com>
2021-07-10 08:46:55 +0200

    added more tasks

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
     <h1>To Do List</h1>
     <List itemList={["Buy Milk", "Watch Netflix", "Study hard!"]} />
+    <List itemList={["Play VideoGames", "Make Dinner", "Book a trip for Chicago!"]} />
     </div>
   );
 }
